bd59c1f230c20c7c1149d8ce3011d01e08928c90
Sofía <sofiaardila@hotmail.com>
2020-11-15 22:20:59 -0500

    Falta aún mostrar la reservas pero ya se guardan bien. Eso junto con los filtros es lo que falta, cosas que mando ahorita.

--- app/src/main/java/com/example/travelu21/Adapter_Reservas_Cliente.java
+++ app/src/main/java/com/example/travelu21/Adapter_Reservas_Cliente.java
@@ -1,4 +1,31 @@
 package com.example.travelu21;
 
-public class Adapter_Reservas_Cliente {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter_Reservas_Cliente extends RecyclerView.Adapter<Adapter_Reservas_Cliente.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
